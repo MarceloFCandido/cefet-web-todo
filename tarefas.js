4ba64769fd397dffe26973d1352e8a7e2b195586
MarceloFCandido <fcandido.marcelo@gmail.com>
2021-12-13 16:59:10 -0300

    doing exercise 3

--- tarefas.js
+++ tarefas.js
@@ -1,12 +1,12 @@
 class Tarefa {
     constructor(nome, categoria, realizada) {
-        let possibleCategories = ["lazer", "compras", "estudo"]
+        let possibleCategories = ["lazer", "compras", "estudos"]
 
         if (typeof (nome) !== "string") {
             throw new Error("Nome não é string.")
         }
         if (!possibleCategories.includes(categoria)) {
-            throw new Error("Categoria inválida.")
+            throw new Error(`Categoria inválida: ${categoria}.`)
         }
         if (typeof (realizada) !== "boolean") {
             throw new Error("\"Realizada\" deve ser booleano.")
@@ -30,11 +30,28 @@ class Tarefa {
     }
 }
 
+
 const tarefas = [
     new Tarefa("Passear com o cachorro", "lazer", false),
     new Tarefa("Comprar ovos", "compras", true)
 ]
+tarefas.toPage = function () {
+    const lista = document.getElementById("lista-tarefas")
+    lista.innerHTML = ""
+    this.forEach((tarefa) => tarefa.seInsereNaPagina(lista))
+}
 
-const lista = document.getElementById("lista-tarefas")
-lista.innerHTML = ""
-tarefas.forEach((tarefa) => tarefa.seInsereNaPagina(lista))
+window.onload = () => tarefas.toPage()
+
+document
+    .getElementById("incluir-nova-tarefa")
+    .addEventListener("click", function (e) {
+        const campoNomeTarefa = document.getElementById("nova-tarefa-nome")
+        const categoriaTarefa = document.getElementById("nova-tarefa-categoria").value
+
+        tarefas.push(new Tarefa(campoNomeTarefa.value, categoriaTarefa, false))
+        tarefas.toPage()
+
+        campoNomeTarefa.value = ""
+        campoNomeTarefa.focus()
+    })
